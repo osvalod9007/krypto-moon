c77f948fbab15cdf35962a78744da498a0a57aec
osvaldo9007 <osvaldo.lopez@lighthousetech.io>
2024-02-27 18:35:31 -0500

    Fixed show articles

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,10 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Krypto Moon';
+export const SITE_DESCRIPTION =
+  'Welcome to Krypto Moon! Immerse yourself in the fascinating world of cryptocurrencies with our platform dedicated to reviewing and promoting various opportunities to earn cryptocurrencies passively. From mining strategies to options with zero initial investment, we will explore all possibilities for you to start your journey into cryptocurrencies from scratch. Discover detailed reviews, effective strategies, and the best platforms to kickstart your path to financial freedom in the digital universe. Join us at Krypto Moon and make your investment begin without spending a penny!';
+export const HOMEPAHE_TITLE = 'No-Investment Guide to Earn Kryptocoins';
+
+export const HOMEPAGE_LIMIT = 7;
+export const ARTICLES_PER_PAGE = 6;
